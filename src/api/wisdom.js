e26b3afb3fc05d6ec0d6aeed86dba637417cccae
sunss <1400205296@qq.com>
2017-08-28 14:14:44 +0800

    api_service

--- src/api/wisdom.js
+++ src/api/wisdom.js
@@ -18,3 +18,67 @@ export function updateSettings(data) {
     data
   })
 }
+//智慧营业区获取早会流程 当前时间的后7天的
+export function earlyMettingList(data) {
+  return fetch({
+    url:port_wisdom.earlyMettingList,
+    method: 'post',
+    data
+  })
+}
+//更新或者新增，删除写flag=-1
+export function saveEarlyMetting(data) {
+  return fetch({
+    url:port_wisdom.saveEarlyMetting,
+    method: 'post',
+    data
+  })
+}
+//获得相册列表
+export function albumList(data) {
+  return fetch({
+    url:port_wisdom.albumList,
+    method: 'post',
+    data
+  })
+}
+//新增或者保存
+export function saveAlbum(data) {
+  return fetch({
+    url:port_wisdom.saveAlbum,
+    method: 'post',
+    data
+  })
+}
+//删除相册
+export function delAlbum(data) {
+  return fetch({
+    url:port_wisdom.delAlbum,
+    method: 'post',
+    data
+  })
+}
+//返回照片列表 -- 无分页
+export function photoList(data) {
+  return fetch({
+    url:port_wisdom.photoList,
+    method: 'post',
+    data
+  })
+}
+//保存相片--批量，单张，删除传flag=-1
+export function savePhoto(data) {
+  return fetch({
+    url:port_wisdom.savePhoto,
+    method: 'post',
+    data
+  })
+}
+//一键清除所有的相片名称
+export function cleanPhotoName(data) {
+  return fetch({
+    url:port_wisdom.cleanPhotoName,
+    method: 'post',
+    data
+  })
+}
